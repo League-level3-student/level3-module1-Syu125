137d61faa4f6bcbedc19a104ebbfacf1f9d62ae7
Syu125 <league@WTS13.attlocal.net>
2018-11-09 16:55:56 -0800

    Finished TestMatchingBrackets; Started Hangman (completed Step1)

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -3,7 +3,10 @@ package _03_IntroToStacks;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.awt.event.KeyEvent;
 import java.util.Stack;
+
+import javax.swing.JOptionPane;
 
 import org.junit.Test;
 
@@ -18,14 +21,23 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		Stack <String> bracket = new Stack <String>();
-		bracket.add(b);
-		for(int i = 0; i < bracket.size(); i++) {
-			
+		Stack<String> bracket = new Stack<String>();
+		for (int i = 0; i < b.length(); i++) {
+			String b1 = String.valueOf(b.charAt(i));
+			if (b1.equals("{")) {
+				bracket.push(b1);
+			}else if (b1.equals("}")) {
+				if(!bracket.isEmpty()) {
+				bracket.pop();
+				}else {
+					return false;
+				}
+			}
 		}
-		return false;
+		return bracket.isEmpty();
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,73 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener{
+	Stack<String> answers = new Stack<String>();
+	JLabel [] labels;
+	String word;
+	String letter;
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+
+	public static void main(String[] args) {
+		HangMan hm = new HangMan();
+		hm.startup();
+		hm.play();
+	}
+
+	void startup() {
+		String words = JOptionPane.showInputDialog(
+				"Hello, welcome to Hangman! How many words would you like to guess? \n(Remember, you lose if you run out of lives)");
+		int w = Integer.parseInt(words);
+		for (int i = 0; i < w; i++) {
+			word = Utilities.readRandomLineFromFile("dictionary.txt");
+			answers.push(word);
+		}
+	}
+
+	void play() {
+		String Word = answers.pop();
+		int characters = Word.length();
+		labels = new JLabel [characters];
+		f.add(p);
+		
+		f.setVisible(true);
+		p.setVisible(true);
+		for(int i = 0; i < characters; i++) {
+			System.out.println("hi");
+			p.add(labels[i]);
+			labels[i].setVisible(true);
+			labels[i].setText("_ ");
+			
+		}
+	}
 	
-	
-	
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		letter = String.valueOf(e.getKeyChar());
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
