f62e7744e88d27d2c5f8b2c240706243108b0295
1luckybiscuit <lukasnepo11@gmail.com>
2018-11-15 16:56:23 -0800

    Hangman
    Finished most of the Hangman, but still need to add in lives. Finished Step 3.

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -10,13 +10,18 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan implements KeyListener{
+public class HangMan implements KeyListener {
 	Stack<String> answers = new Stack<String>();
-	JLabel [] labels;
+	JLabel[] labels;
+	JLabel[] labels2;
 	String word;
-	String letter;
+	Character letter;
 	JFrame f = new JFrame();
 	JPanel p = new JPanel();
+	Character[] c;
+	int count;
+	int characters;
+	Character [] overlap;
 
 	public static void main(String[] args) {
 		HangMan hm = new HangMan();
@@ -35,39 +40,69 @@ public class HangMan implements KeyListener{
 	}
 
 	void play() {
-		String Word = answers.pop();
-		int characters = Word.length();
-		labels = new JLabel [characters];
+		word = answers.pop();
+		characters = word.length();
+		labels = new JLabel[characters];
+		labels2 = new JLabel[characters];
 		f.add(p);
-		
 		f.setVisible(true);
 		p.setVisible(true);
-		for(int i = 0; i < characters; i++) {
-			System.out.println("hi");
+		f.addKeyListener(this);
+		c = new Character[characters];	
+		System.out.println(characters);
+		for (int i = 0; i < characters; i++) {
+			labels[i] = new JLabel();
+			labels2[i] = new JLabel();
 			p.add(labels[i]);
+			c[i] = word.charAt(i);
+			labels2[i].setText(c[i].toString());
+			labels[i].setText("_");	
 			labels[i].setVisible(true);
-			labels[i].setText("_ ");
-			
+		
 		}
+		f.pack();
 	}
-	
+
+	void match() {
+
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+	
 		// TODO Auto-generated method stub
-		letter = String.valueOf(e.getKeyChar());
+	
 	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	letter = e.getKeyChar();
+	overlap = new Character[characters];
+		for (int i = 0; i < characters; i++) {
+			if (letter.equals(c[i])) {
+			overlap[i] = new Character(letter);
+				labels[i].setText(labels2[i].getText());
+				System.out.println("hi");
+				for(int j = 0; j < characters; j++) {
+				if(!letter.equals(overlap[i])) {
+					count+=1;
+				}
+				}
+			}
+				
+			if(count == characters) {
+				play();
+			}
+		}
+
 	}
+	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
